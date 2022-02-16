fd841f326a74ad0f5744069d69beb882cf92682f
Luis Garcia <32013748+LuisEGarciaC@users.noreply.github.com>
2022-02-16 18:34:40 -0400

    Actualización de datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/luisenriquegarciacontreras9@gmail.com", {
+    fetch("https://formsubmit.co/ajax/luisegarciac911@gmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
