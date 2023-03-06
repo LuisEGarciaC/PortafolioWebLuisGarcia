a53f97f379b1735f3b160921891d48ef7f1b7aff
Luis <luigui.hades@gmail.com>
2023-03-06 11:45:29 -0400

    actualizacion de js

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,29 +27,29 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/luisegarciac911@gmail.com", {
-      method: "POST",
-      body: new FormData(e.target),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((json) => {
-        console.log(json);
-        location.hash = "#gracias";
-        $form.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-        let message =
-          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
-        $response.querySelector(
-          "h3"
-        ).innerHTML = `Error ${err.status}: ${message}`;
-      })
-      .finally(() => {
-        $loader.classList.add("none");
-        setTimeout(() => {
-          location.hash = "#close";
-        }, 3000);
-      });
+    fetch("https://formsubmit.co/ajax/c0610ad7007aafe6de99238ff6472f76", {
+			method: "POST",
+			body: new FormData(e.target),
+		})
+			.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+			.then((json) => {
+				console.log(json);
+				location.hash = "#gracias";
+				$form.reset();
+			})
+			.catch((err) => {
+				console.log(err);
+				let message =
+					err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+				$response.querySelector(
+					"h3"
+				).innerHTML = `Error ${err.status}: ${message}`;
+			})
+			.finally(() => {
+				$loader.classList.add("none");
+				setTimeout(() => {
+					location.hash = "#close";
+				}, 3000);
+			});
   });
 })(document);
